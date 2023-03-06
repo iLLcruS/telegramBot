4996950838660b84e8e9ab04649d321c41a26b3b
vladmacbook <rostovskii.1030@gmail.com>
2023-03-06 23:38:20 +0200

    Edit config-Bot file

--- src/main/java/com/converterdoit/telegramBot/config/configBot.java
+++ src/main/java/com/converterdoit/telegramBot/config/configBot.java
@@ -1,6 +1,18 @@
 package com.converterdoit.telegramBot.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
 public class configBot {
+
+    @Value("${bot.name}")
     String botName;
+
+    @Value("${bot.token}")
     String token;
 }
